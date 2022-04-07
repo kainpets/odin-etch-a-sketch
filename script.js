4b76290fa937f3d2f90e0537a18bf3962b5a57cf
kainpets <pawelstepniak22@gmail.com>
2022-04-07 23:34:22 +0200

    the sketchpad logic works and size can be set

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 const container = document.querySelector("#container");
 const clearButton = document.querySelector("#clear-btn");
 const newCanvasButton = document.querySelector("#new-canvas-btn");
+const everything = document.querySelector(".everything");
 
+const createSquares = numOfSquares => {
+  container.style.gridTemplateColumns = `repeat(${numOfSquares}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${numOfSquares}, 1fr)`;
+  
+  
+  for (let i = 0; i < numOfSquares**2; i++) {
+    let div = document.createElement("div");
+    div.classList.add("squares");
+    container.appendChild(div);
+    const squares = document.querySelectorAll(".squares");
+    
+    squares.forEach(square => {
+      square.addEventListener("mouseover", event => {
+        event.target.style.background = "red";
+      });
+    })
+  }
+} 
 
-for (let i = 0; i < 256; i++) {
-  let div = document.createElement("div");
-  div.classList.add("squares");
-  container.appendChild(div);
+const changeSize = size => {
+  if(size >= 2 && size < 100) {
+    const squares = document.querySelectorAll(".squares");
+    squares.forEach(square => square.remove());
+    createSquares(size);
+  } else {
+    const errorMessage = document.createElement("div");
+    errorMessage.textContent = "Set a value from 2 to 100";
+    everything.appendChild(errorMessage);
+  }
 }
-
-container.style.display = "grid";
-container.style.justifyContent = "center";
-container.style.gridTemplateColumns = "repeat(16, 10px)";
-container.style.gridTemplateRows = "repeat(16, 10px)";
-container.style.gridColumnGap = "0px";
-container.style.gridRowGap = "0px";
-container.style.border = "dashed";
-
-const squares = document.querySelectorAll(".squares");
-
-squares.forEach(square => {
-  square.addEventListener("mouseover", (event) => {
-    event.target.style.background = "red";
-  });
-})
 
 clearButton.addEventListener("click", () => {
   squares.forEach(square => {
@@ -31,27 +40,4 @@ clearButton.addEventListener("click", () => {
   });
 })
 
-newCanvasButton.addEventListener("click", () => {
-  container.remove()
-  const newContainer = document.querySelector("#new-container")
-  
-  let size = prompt("Enter the size of your canvas: (up to 100)", "16");
-  Number.parseInt(size, 10);
-
-
-  for (let i = 0; i < size * size; i++) {
-    let div = document.createElement("div");
-    div.classList.add("newSquares");
-    newContainer.appendChild(div);
-  }
-
-  newContainer.style.display = "grid";
-  newContainer.style.justifyContent = "center";
-  newContainer.style.gridTemplateColumns = "repeat(16, 10px)";
-  newContainer.style.gridTemplateRows = "repeat(16, 10px)";
-  newContainer.style.gridColumnGap = "0px";
-  newContainer.style.gridRowGap = "0px";
-  newContainer.style.border = "dashed";
-
-})
-  
+createSquares(16);
